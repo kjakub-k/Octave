48f3186012729949a4780d135e41e044cba7845b
Jakub <kjakub.dev@gmail.com>
2025-11-25 20:06:46 +0100

    changed the way a hit accuracy is shown

--- Assets/Scripts/UI/Game/GameUI.cs
+++ Assets/Scripts/UI/Game/GameUI.cs
@@ -11,27 +11,34 @@ namespace KJakub.Octave.UI.Game
         [Header("Managers")]
         [SerializeField]
         private GameController gameController;
+        private LatestAccuracyUI latestAccuracyUI;
         private Label scoreLabel;
         private Label comboLabel;
         private Label highestComboLabel;
-        private VisualElement accuracyContainer;
         private void Start()
         {
             var root = GetComponent<UIDocument>().rootVisualElement;
             InitializeElements(root);
 
             gameController.GameStats.OnComboChanged += UpdateCombo;
-            gameController.GameStats.OnHit += (AccuracySO acc) => SpawnAccuracy(acc.Title, acc.Weight * 10);
+            gameController.GameStats.OnHit += (AccuracySO acc) =>
+            {
+                latestAccuracyUI.ShowLabel(acc.Title + "!");
+                UpdateScore(gameController.GameStats.Score);
+            };
             gameController.GameStats.OnReset += ResetUI;
-            gameController.GameStats.OnMiss += () => UpdateScore(gameController.GameStats.Score);
-            gameController.GameStats.OnMiss += () => SpawnAccuracy("Miss", -10);
+            gameController.GameStats.OnMiss += () =>
+            {
+                latestAccuracyUI.ShowLabel("Miss");
+                UpdateScore(gameController.GameStats.Score);
+            };
         }
         private void InitializeElements(VisualElement root)
         {
             scoreLabel = root.Q<Label>("Score");
             comboLabel = root.Q<Label>("Combo");
             highestComboLabel = root.Q<Label>("HighestCombo");
-            accuracyContainer = root.Q<VisualElement>("AccuracyContainer");
+            latestAccuracyUI = new(root.Q<Label>("Accuracy"));
         }
         private void UpdateScore(int score)
         {
@@ -42,24 +49,10 @@ namespace KJakub.Octave.UI.Game
             comboLabel.text = combo.ToString();
             highestComboLabel.text = highestCombo.ToString();
         }
-        private void SpawnAccuracy(string title, int score)
-        {
-            VisualElement accElement = new();
-            Label label = new();
-            accElement.AddToClassList("accuracy");
-            accElement.AddToClassList($"{title.ToLower()}");
-            label.text = $"{title} ({((score > 0) ? '+' + score.ToString() : score.ToString())})";
-            accElement.Add(label);
-
-            accuracyContainer.Add(accElement);
-
-            UpdateScore(gameController.GameStats.Score);
-        }
         private void ResetUI()
         {
             UpdateCombo(0, 0);
             UpdateScore(0);
-            accuracyContainer.Clear();
         }
     }
 }
---
+++ Assets/Scripts/UI/Game/LatestAccuracyUI.cs
@@ -0,0 +1,37 @@
+using UnityEngine.UIElements;
+namespace KJakub.Octave.UI.Game
+{
+    public class LatestAccuracyUI
+    {
+        private Label accuracyLabel;
+        public LatestAccuracyUI(Label accuracyLabel)
+        {
+            this.accuracyLabel = accuracyLabel;
+        }
+        public void ShowLabel(string title)
+        {
+            accuracyLabel.text = title;
+            accuracyLabel.RemoveFromClassList("show");
+            accuracyLabel.RemoveFromClassList("hidden");
+
+            accuracyLabel.schedule.Execute(() =>
+            {
+                accuracyLabel.AddToClassList("hidden");
+
+                accuracyLabel.schedule.Execute(() =>
+                {
+                    accuracyLabel.RemoveFromClassList("hidden");
+                    accuracyLabel.AddToClassList("show");
+
+                }).StartingIn(1);
+
+            }).StartingIn(1);
+
+            accuracyLabel.schedule.Execute(() =>
+            {
+                accuracyLabel.RemoveFromClassList("show");
+                accuracyLabel.AddToClassList("hidden");
+            }).StartingIn(700);
+        }
+    }
+}
